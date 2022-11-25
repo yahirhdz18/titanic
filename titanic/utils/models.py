ef1f4ae37fa3fd80b8835d67df2827b66756d473
yahirhdz18 <yahir_hm18@hotmail.com>
2022-11-25 03:10:46 -0600

    add Passanger model

---
+++ titanic/utils/models.py
@@ -0,0 +1,65 @@
+#Python
+from typing import Optional
+from enum import Enum
+
+#Pydantic
+from pydantic import BaseModel
+from pydantic import Field
+
+class Sex(Enum):
+    female= "female"
+    male= "male"
+
+class Embarked(Enum):
+    S ='S' 
+    C = 'C'
+    Q = 'Q'
+
+class Passager(BaseModel):
+    name: str = Field(
+        ...,
+        min_length = 1,
+        max_length=50,
+        example="Master Yahir"
+        )
+    sex: Sex = Field(
+        ...,
+        example = 'male'
+        )
+    age: int = Field(
+        ...,
+        ge=0,
+        le=100,
+        example = 26
+        )
+    pclass: int = Field(
+        ...,
+        ge = 1,
+        le = 3,
+        example = 1
+        )
+    sibsp: int = Field(
+        ...,
+        ge = 0,
+        le = 8,
+        example = 2
+        )
+    parch: int = Field(
+        ...,
+        ge = 0,
+        le = 9,
+        example = 2
+        )
+    fare: float = Field(
+        ...,
+        ge = 0,
+        le = 600,
+        example = 500
+        )
+    cabin: Optional[str] = Field(
+        example = 'C202'
+        )
+    embarked: Optional[Embarked] = Field(
+        example = 'S'
+        )
+    
