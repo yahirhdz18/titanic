24e52d1ee989879d8d4e696904a87897c2d13f49
yahirhdz18 <yahir_hm18@hotmail.com>
2022-11-25 16:42:26 -0600

    remove non used libraries test_transformers.py

--- tests/unit/test_transformers.py
+++ tests/unit/test_transformers.py
@@ -3,8 +3,6 @@ import numpy as np
 
 import pandas as pd
 
-import pytest
-
 sys.path.append('./') 
 from titanic.utils.transformers import ReplaceQM
 from titanic.utils.transformers import CleanData
